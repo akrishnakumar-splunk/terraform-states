0d7c3b6c62b7a98010f3987734ba6afff000364b
akrishnakumar-splunk <akrishnakumar@splunk.com>
2018-07-29 10:30:08 +0530

    updating jenkinsfile for build

--- provider.tf
+++ provider.tf
@@ -1,8 +1,6 @@
 variable "vsphere_user" {}
 variable "vsphere_password" {}
 variable "vsphere_server" {}
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
 variable "instance_name" {}
 
 variable "cpu_count" {
@@ -25,11 +23,4 @@ provider "vsphere" {
 
   # If you have a self-signed cert
   allow_unverified_ssl = true
-}
-
-provider "aws" {
-  version    = "~> 1.29"
-  region     = "us-east-1"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
 }
